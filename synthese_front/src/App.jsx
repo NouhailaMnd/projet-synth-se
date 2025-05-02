19dce8d2a25529d6c71c7667f60790998c5c8084
kawtar-jrn <jarnijakawtar12@123#123>
2025-05-02 11:11:10 +0100

    Initial commit: Laravel backend + React frontend

---
+++ synthese_front/src/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from "./layouts/navBar"
+import Register from "./components/register"
+
+function App() {
+
+  return (
+    <>
+    <NavBar/>
+    <Register/>
+     <div></div>
+    </>
+  )
+}
+
+export default App
---
+++ sythese-project/app/Http/Controllers/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $fields = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|max:255',
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']),
+        ]);
+
+        if (!$user){
+            return [
+                'message' => 'User not created.'
+            ];
+        }
+
+        
+
+        $token = $user->createToken($request->name);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
+    }
+
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)){
+            return [
+                'message' => 'the provided credentials are incorrect.'
+            ];
+        }
+
+        $token = $user->createToken($user->name);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
+    }
+
+    public function logout(Request $request){
+        $request->user()->tokens()->delete();
+
+        return [
+            'message' => 'You are logged out.'
+        ];
+    }
+}
